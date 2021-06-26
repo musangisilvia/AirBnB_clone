e5254b42d95283fa1967376b864d5e1ed3a27510
musangisilvia <musangisilvia@gmail.com>
2021-06-26 12:16:13 +0000

    add show and create functions

--- console.py
+++ console.py
@@ -4,6 +4,8 @@
 """
 
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +14,62 @@ class HBNBCommand(cmd.Cmd):
         It is a command  interpreter for the AirBnB project.
     """
     prompt = '(hbnb) '
+
+    def do_create(self, line):
+        """
+            Creates a new instance of BaseModel, saves it to the JSON file
+            prints the id.
+            Usage: create <ClassName>
+        """
+
+        cmds = line.split()
+
+        if len(cmds) is 0:
+            print("** class name missing **")
+            return
+
+        if cmds[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+
+        newModel = BaseModel()
+        newModel.save()
+        print(newModel.id)
+
+    def do_show(self, line):
+        """
+            Prints the string representation of an instance based
+            on the class name and id.
+            Usage: show <ClassName> <id>
+        """
+
+        cmds = line.split()
+
+        if len(cmds) == 0:
+            print("** class name missing **")
+            return
+
+        if len(cmds) == 1:
+            print("** instance id missing **")
+            return
+
+        storage = FileStorage()
+        storage.reload()
+        obj = storage.all()
+
+        try:
+            cmds[0]
+        except NameError:
+            print("** class doesn't exist **")
+            return
+
+        key = cmds[0] + "." + cmds[1]
+
+        try:
+            value = obj[key]
+            print(value)
+        except KeyError:
+            print("** no instance found **")
 
     def emptyline(self):
         """
@@ -32,8 +90,18 @@ class HBNBCommand(cmd.Cmd):
         """
         return True
 
+# Help functions
+
+    def help_create(self):
+        print("Creates a new instance of BaseModel and saves \
+it.\nUsage: create <ClassName>")
+
+    def help_show(self):
+        print("Prints the string representation of an instance \
+based on the class name and id.\nUsage: show <ClassName> <id>.")
+
     def help_quit(self):
-        print("Exit the command line")
+        print("Exit the command line.\nUsage: quit")
 
     def help_EOF(self):
         print("Exit the command line")
