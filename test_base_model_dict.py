003d8bc2cf9d5d2328053205596876596d29fd5e
musangisilvia <musangisilvia@gmail.com>
2021-06-29 11:23:34 +0000

    fix pep8 errors

--- test_base_model_dict.py
+++ test_base_model_dict.py
@@ -8,14 +8,15 @@ print(my_model.id)
 print(my_model)
 print(type(my_model.created_at))
 print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
+model_json = my_model.to_dict()
+print(model_json)
 print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+for key in model_json.keys():
+    print("\t{}: ({}) - {}".format(key,
+                                   type(model_json[key]), model_json[key]))
 
 print("--")
-my_new_model = BaseModel(**my_model_json)
+my_new_model = BaseModel(**model_json)
 print(my_new_model.id)
 print(my_new_model)
 print(type(my_new_model.created_at))
